f91b4f379ffd76f9d74286db68b01bed01418195
root <artym13@gmail.com>
2016-04-27 19:12:24 +0300

    Fixed bug with photo loading

--- frontend/app/scripts/services/vk.js
+++ frontend/app/scripts/services/vk.js
@@ -3,6 +3,7 @@
 angular.module('myAppApp').factory('VKService',
   ['$cookies', '$location', 'APIService', '$rootScope', function($cookies, $location, APIService, $rootScope) {
       VK.init({ apiId: 5360849 });
+      var loadSemaphore = false;
 
       return {
         LogInEvent: function(){
@@ -15,13 +16,15 @@ angular.module('myAppApp').factory('VKService',
 
             APIService.login('vk', response.session.user.id).then(function(resp){
               $rootScope.uid = resp.data.data;
-              document.location = '/#/';
-
+              if(loadSemaphore) document.location = '/#/';
+              else loadSemaphore = true;
             });
 
             VK.Api.call('users.get', {uids: response.session.user.id, fields: "photo_200_orig"}, function(r) {
               if(r.response) {
                 $rootScope.uphoto = r.response[0].photo_200_orig;
+                if(loadSemaphore) document.location = '/#/';
+                else loadSemaphore = true;
               }
             });
 
